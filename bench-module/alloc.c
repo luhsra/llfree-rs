8501b2d12de299e84a1c3390b23b484bc121fb81
Lars Wrenger <wrenger@sra.uni-hannover.de>
2022-08-23 18:28:52 +0200

    :sparkles: Fix bench module thread cleanup

--- bench-module/alloc.c
+++ bench-module/alloc.c
@@ -19,10 +19,6 @@ MODULE_AUTHOR("Lars Wrenger");
 #define NUM_ALLOCS (512 * 512)
 #endif
 
-#ifndef THREADS_MAX
-#define THREADS_MAX 6UL
-#endif
-
 #ifndef ITERATIONS
 #define ITERATIONS 4
 #endif
@@ -31,17 +27,19 @@ static const u64 threads[] = { 1,  2,  4,  8,  16, 20, 24,
 			       32, 40, 48, 56, 64, 80, 96 };
 #define THREADS_LEN (sizeof(threads) / sizeof(*threads))
 
-static struct task_struct *tasks[THREADS_MAX];
+static struct task_struct **tasks = NULL;
+
 static DECLARE_COMPLETION(start_barrier);
 static DECLARE_COMPLETION(mid_barrier);
-static struct completion barriers[THREADS_MAX];
+
+static DEFINE_PER_CPU(struct completion, barriers);
 static bool running = false;
 
 struct thread_perf {
 	atomic64_t get;
 	atomic64_t put;
 };
-static struct thread_perf thread_perf[THREADS_MAX];
+static DEFINE_PER_CPU(struct thread_perf, thread_perf);
 
 struct perf {
 	u64 get_min;
@@ -76,6 +74,8 @@ __maybe_unused static void bulk(u64 tid)
 {
 	u64 j;
 	u64 timer;
+	struct completion *barrier = this_cpu_ptr(&barriers);
+	struct thread_perf *t_perf = this_cpu_ptr(&thread_perf);
 	struct page **pages =
 		kmalloc_array(NUM_ALLOCS, sizeof(struct page *), GFP_KERNEL);
 
@@ -85,7 +85,7 @@ __maybe_unused static void bulk(u64 tid)
 	}
 
 	// complete initialization
-	complete(&barriers[tid]);
+	complete(barrier);
 
 	// Start allocations
 	wait_for_completion(&start_barrier);
@@ -98,10 +98,10 @@ __maybe_unused static void bulk(u64 tid)
 		}
 	}
 	timer = (ktime_get_ns() - timer) / NUM_ALLOCS;
-	atomic64_set(&thread_perf[tid].get, timer);
+	atomic64_set(&t_perf->get, timer);
 	pr_info("Alloc %llu\n", timer);
 
-	complete(&barriers[tid]);
+	complete(barrier);
 
 	// Start frees
 	wait_for_completion(&mid_barrier);
@@ -111,7 +111,7 @@ __maybe_unused static void bulk(u64 tid)
 		__free_page(pages[j]);
 	}
 	timer = (ktime_get_ns() - timer) / NUM_ALLOCS;
-	atomic64_set(&thread_perf[tid].put, timer);
+	atomic64_set(&t_perf->put, timer);
 	pr_info("Free %llu\n", timer);
 	kfree(pages);
 }
@@ -121,11 +121,13 @@ __maybe_unused static void repeat(u64 tid)
 {
 	u64 j;
 	u64 timer;
+	struct completion *barrier = this_cpu_ptr(&barriers);
+	struct thread_perf *t_perf = this_cpu_ptr(&thread_perf);
 
 	struct page *page;
 
 	// complete initialization
-	complete(&barriers[tid]);
+	complete(barrier);
 
 	// Start reallocs
 	wait_for_completion(&mid_barrier);
@@ -139,9 +141,9 @@ __maybe_unused static void repeat(u64 tid)
 		__free_page(page);
 	}
 	timer = (ktime_get_ns() - timer) / NUM_ALLOCS;
-	atomic64_set(&thread_perf[tid].get, timer);
-	atomic64_set(&thread_perf[tid].put, timer);
-	pr_info("Realloc %llu\n", timer);
+	atomic64_set(&t_perf->get, timer);
+	atomic64_set(&t_perf->put, timer);
+	pr_info("Repeat %llu\n", timer);
 }
 
 /// Random free and realloc
@@ -150,6 +152,8 @@ __maybe_unused static void rand(u64 tid)
 	u64 i, j;
 	u64 timer;
 	u64 rng = tid;
+	struct completion *barrier = this_cpu_ptr(&barriers);
+	struct thread_perf *t_perf = this_cpu_ptr(&thread_perf);
 
 	struct page **pages =
 		kmalloc_array(NUM_ALLOCS, sizeof(struct page *), GFP_KERNEL);
@@ -166,7 +170,7 @@ __maybe_unused static void rand(u64 tid)
 	}
 
 	// complete initialization
-	complete(&barriers[tid]);
+	complete(barrier);
 
 	// Start reallocs
 	wait_for_completion(&mid_barrier);
@@ -182,20 +186,21 @@ __maybe_unused static void rand(u64 tid)
 		}
 	}
 	timer = (ktime_get_ns() - timer) / NUM_ALLOCS;
-	atomic64_set(&thread_perf[tid].get, timer);
-	atomic64_set(&thread_perf[tid].put, timer);
+	atomic64_set(&t_perf->get, timer);
+	atomic64_set(&t_perf->put, timer);
 
 	for (j = 0; j < NUM_ALLOCS; j++) {
 		__free_page(pages[j]);
 	}
 
-	pr_info("Realloc %llu\n", timer);
+	pr_info("Rand %llu\n", timer);
 	kfree(pages);
 }
 
 static int worker(void *data)
 {
 	struct work_data d = { .value = (u64)data };
+	struct completion *barrier = this_cpu_ptr(&barriers);
 
 	pr_info("Worker %u bench %u\n", d.tid, d.bench);
 
@@ -207,7 +212,7 @@ static int worker(void *data)
 		rand(d.tid);
 	}
 
-	complete(&barriers[d.tid]);
+	complete(barrier);
 
 	return 0;
 }
@@ -230,7 +235,7 @@ static ssize_t out_show(struct kobject *kobj, struct kobj_attribute *attr,
 	}
 
 	for (ssize_t i = out_index;
-	     i < THREADS_LEN && threads[i] <= THREADS_MAX; i++) {
+	     i < THREADS_LEN && threads[i] <= num_possible_cpus(); i++) {
 		// The output buffer has only the size of a PAGE.
 		// If our output is larger we have to output it in multiple steps.
 		if (len < PAGE_SIZE - ITERATIONS * 128) {
@@ -254,101 +259,119 @@ static ssize_t out_show(struct kobject *kobj, struct kobj_attribute *attr,
 	return len;
 }
 
-ssize_t run_store(struct kobject *kobj, struct kobj_attribute *attr,
-		  const char *buf, size_t len)
+ssize_t iteration(u32 bench, u64 i, u64 iter)
+{
+	struct perf *p;
+	u64 get, put;
+
+	pr_info("Start threads %llu\n", threads[i]);
+	for (u64 t = 0; t < threads[i]; t++) {
+		struct completion *barrier = per_cpu_ptr(&barriers, t);
+
+		struct work_data data = {
+			.tid = t,
+			.bench = bench,
+		};
+		tasks[t] = kthread_create_on_cpu(worker, (void *)data.value, t,
+						 "worker");
+		if (IS_ERR(tasks[t])) {
+			pr_err("Unable to init %llu\n", t);
+			return PTR_ERR(tasks[t]);
+		}
+		init_completion(barrier);
+		wake_up_process(tasks[t]);
+	}
+
+	// Init
+	for (u64 t = 0; t < threads[i]; t++) {
+		struct completion *barrier = per_cpu_ptr(&barriers, t);
+		wait_for_completion(barrier);
+		reinit_completion(barrier);
+	}
+
+	pr_info("Exec %llu threads\n", threads[i]);
+	p = &perf[i * ITERATIONS + iter];
+	p->get_min = (u64)-1;
+	p->get_avg = 0;
+	p->get_max = 0;
+	p->put_min = (u64)-1;
+	p->put_avg = 0;
+	p->put_max = 0;
+
+	// bulk alloc has two phases (alloc and free)
+	if (bench == 0) {
+		complete_all(&start_barrier);
+
+		pr_info("Waiting for workers...\n");
+
+		for (u64 t = 0; t < threads[i]; t++) {
+			struct completion *barrier = per_cpu_ptr(&barriers, t);
+			wait_for_completion(barrier);
+			reinit_completion(barrier);
+		}
+	}
+
+	// second phase
+	complete_all(&mid_barrier);
+
+	for (u64 t = 0; t < threads[i]; t++) {
+		struct completion *barrier = per_cpu_ptr(&barriers, t);
+		struct thread_perf *t_perf = per_cpu_ptr(&thread_perf, t);
+
+		wait_for_completion(barrier);
+		reinit_completion(barrier);
+		get = atomic64_read(&t_perf->get);
+		put = atomic64_read(&t_perf->put);
+
+		p->get_min = min(p->get_min, get);
+		p->get_avg += get;
+		p->get_max = max(p->get_max, get);
+		p->put_min = min(p->put_min, put);
+		p->put_avg += put;
+		p->put_max = max(p->put_max, put);
+	}
+	p->get_avg /= threads[i];
+	p->put_avg /= threads[i];
+
+	// Cleanup
+	reinit_completion(&start_barrier);
+	reinit_completion(&mid_barrier);
+	// for (u64 t = 0; t < threads[i]; t++) {
+	// 	kthread_stop(tasks[t]);
+	// }
+	return 0;
+}
+
+static ssize_t run_store(struct kobject *kobj, struct kobj_attribute *attr,
+			 const char *buf, size_t len)
 {
 	u32 bench = 0;
+
+	if (len == 0 || len > 10 || buf == NULL)
+		return -EINVAL;
+
+	if (strncmp(buf, "bulk", 1) == 0) {
+		bench = 0;
+	} else if (strncmp(buf, "repeat", 1) == 0) {
+		bench = 1;
+	} else if (strncmp(buf, "rand", 1) == 0) {
+		bench = 2;
+	} else {
+		pr_err("Invalid mode %s", buf);
+		return -EINVAL;
+	}
 
 	if (running)
 		return -EINPROGRESS;
 	running = true;
 
-	if (len == 0 || len > 1 || buf == NULL)
-		return -EINVAL;
-
-	if (strncmp(buf, "bulk", 1)) {
-		bench = 0;
-	} else if (strncmp(buf, "repeat", 1)) {
-		bench = 1;
-	} else if (strncmp(buf, "rand", 1)) {
-		bench = 2;
-	} else {
-		return -EINVAL;
-	}
-
-	for (u64 i = 0; i < THREADS_LEN && threads[i] <= THREADS_MAX; i++) {
+	for (u64 i = 0; i < THREADS_LEN && threads[i] <= num_possible_cpus();
+	     i++) {
 		for (u64 iter = 0; iter < ITERATIONS; iter++) {
-			struct perf *p;
-			u64 get, put;
-
-			pr_info("Start threads %llu\n", threads[i]);
-			for (u64 t = 0; t < threads[i]; t++) {
-				struct work_data data = {
-					.tid = t,
-					.bench = bench,
-				};
-				tasks[t] = kthread_create(
-					worker, (void *)data.value, "worker");
-				if (IS_ERR(tasks[t])) {
-					pr_err("Unable to init %llu\n", t);
-					return PTR_ERR(tasks[t]);
-				}
-				kthread_bind(tasks[t], t);
-				init_completion(&barriers[t]);
-				wake_up_process(tasks[t]);
-			}
-
-			// Init
-			for (u64 t = 0; t < threads[i]; t++) {
-				wait_for_completion(&barriers[t]);
-				reinit_completion(&barriers[t]);
-			}
-
-			pr_info("Exec %llu threads\n", threads[i]);
-			p = &perf[i * ITERATIONS + iter];
-			p->get_min = (u64)-1;
-			p->get_avg = 0;
-			p->get_max = 0;
-			p->put_min = (u64)-1;
-			p->put_avg = 0;
-			p->put_max = 0;
-
-			// bulk alloc has two phases (alloc and free)
-			if (bench == 0) {
-				complete_all(&start_barrier);
-
-				pr_info("Waiting for workers...\n");
-
-				for (u64 t = 0; t < threads[i]; t++) {
-					wait_for_completion(&barriers[t]);
-					reinit_completion(&barriers[t]);
-				}
-			}
-
-			// second phase
-			complete_all(&mid_barrier);
-
-			for (u64 t = 0; t < threads[i]; t++) {
-				wait_for_completion(&barriers[t]);
-				reinit_completion(&barriers[t]);
-				get = atomic64_read(&thread_perf[t].get);
-				put = atomic64_read(&thread_perf[t].put);
-
-				p->get_min = min(p->get_min, get);
-				p->get_avg += get;
-				p->get_max = max(p->get_max, get);
-				p->put_min = min(p->put_min, put);
-				p->put_avg += put;
-				p->put_max = max(p->put_max, put);
-			}
-			p->get_avg /= threads[i];
-			p->put_avg /= threads[i];
-
-			// Cleanup
-			reinit_completion(&start_barrier);
-			reinit_completion(&mid_barrier);
-			for (u64 t = 0; t < threads[i]; t++) {
-				kthread_stop(tasks[t]);
+			ssize_t ret = iteration(bench, i, iter);
+			if (ret != 0) {
+				running = false;
+				return ret;
 			}
 		}
 	}
@@ -356,11 +379,11 @@ ssize_t run_store(struct kobject *kobj, struct kobj_attribute *attr,
 	pr_info("Finished\n");
 	running = false;
 
-	return 0;
+	return len;
 }
 
 static struct kobj_attribute out_attribute = __ATTR(out, 0444, out_show, NULL);
-static struct kobj_attribute run_attribute = __ATTR(out, 0220, NULL, run_store);
+static struct kobj_attribute run_attribute = __ATTR(run, 0220, NULL, run_store);
 
 static struct attribute *attrs[] = {
 	&out_attribute.attr, &run_attribute.attr,
@@ -389,6 +412,9 @@ static int alloc_init_module(void)
 		kobject_put(output);
 	}
 
+	tasks = kmalloc_array(num_possible_cpus(), sizeof(struct task_struct *),
+			      GFP_KERNEL);
+
 	return 0;
 }
 
@@ -396,6 +422,7 @@ static void alloc_cleanup_module(void)
 {
 	pr_info("End\n");
 	kobject_put(output);
+	kfree(tasks);
 }
 
 module_init(alloc_init_module);
