85cb772e334992856d81679184efc743560aeb18
Lars Wrenger <wrenger@sra.uni-hannover.de>
2022-08-25 15:04:55 +0200

    :wrench: Update benchmark scripts

--- bench/remote.sh
+++ bench/remote.sh
@@ -2,12 +2,12 @@ export RUST_LOG=warn
 mkdir -p bench/out
 set -e
 
-ALLOC="ArrayAtomicC64 ArrayAtomicC128 ArrayAtomicC512 ArrayAtomicA128 ArrayAtomicA256 ArrayAtomicA512"
+ALLOC="ArrayAtomicA128 ArrayAtomicA256 ArrayAtomicA512 ArrayListA128 ArrayListA256 ArrayListA512"
 THREADS="-x1 -x2 -x4 -x8 -x16 -x24 -x32 -x40 -x48 -x56 -x64 -x80 -x96"
 FILL="-x0 -x10 -x20 -x30 -x40 -x50 -x60 -x70 -x80"
 
 # NVRAM
-FLAGS="-t96 -i4 -s0 -m256 --dax=/dev/dax0.1 --stride 2"
+FLAGS="-t96 -i4 -s0 -m192 --dax=/dev/dax0.0 --stride 2"
 
 echo ">>> check p bulk"
 chrt 99 target/release/bench bulk $ALLOC $FLAGS $THREADS \
@@ -18,16 +18,9 @@ chrt 99 target/release/bench repeat $ALLOC $FLAGS $THREADS \
 echo ">>> check p rand"
 chrt 99 target/release/bench rand $FLAGS $ALLOC $THREADS \
     -o results/out/rand_p.csv
-#echo ">>> check p filling"
-#chrt 99 target/release/bench filling -t24 -i4 -s0 -m240 --dax=/dev/dax0.1 $ALLOC $FILL \
-#    -o results/out/filling_p.csv
-
-#echo ">>> check p align"
-#chrt 99 target/release/bench bulk ArrayAlignedD ArrayUnalignedD $FLAGS $THREADS \
-#    -o results/out/align_p.csv
 
 # DRAM
-FLAGS="-t96 -i4 -s0 -m256 --stride 2"
+FLAGS="-t96 -i4 -s0 -m192 --stride 2"
 
 echo ">>> check v bulk"
 chrt 99 target/release/bench bulk $ALLOC $FLAGS $THREADS \
@@ -38,13 +31,6 @@ chrt 99 target/release/bench repeat $ALLOC $FLAGS $THREADS \
 echo ">>> check v rand"
 chrt 99 target/release/bench rand $FLAGS $ALLOC $THREADS \
     -o results/out/rand_v.csv
-#echo ">>> check v filling"
-#chrt 99 target/release/bench filling -t24 -i4 -s0 -m240 $ALLOC $FILL \
-#    -o results/out/filling_v.csv
-
-#echo ">>> check v align"
-#chrt 99 target/release/bench bulk ArrayAlignedD ArrayUnalignedD $FLAGS $THREADS \
-#    -o results/out/align_v.csv
 
 exit 0;
 
