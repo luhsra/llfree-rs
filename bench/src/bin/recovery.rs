701481b140437b432b6b35d7f79027dc07c04558
Lars Wrenger <wrenger@sra.uni-hannover.de>
2023-01-09 22:21:47 +0100

    :sparkles: Improve recovery test

--- bench/src/bin/recovery.rs
+++ bench/src/bin/recovery.rs
@@ -1,13 +1,16 @@
 #![feature(int_roundings)]
 
+use core::result::Result;
 use std::fs::File;
+use std::hint::black_box;
 use std::io::Write;
-use std::time::Instant;
+use std::process::Command;
+use std::time::{Duration, Instant};
 
 use clap::Parser;
 use log::warn;
 
-use nvalloc::lower::{Cache, LowerAlloc};
+use nvalloc::lower::Cache;
 use nvalloc::mmap::MMap;
 use nvalloc::table::PT_LEN;
 use nvalloc::upper::{Alloc, AllocExt, Array, Init};
@@ -30,10 +33,12 @@ struct Args {
     /// Simulate a crash by changing counters and setting the active bit.
     #[arg(short, long)]
     crash: bool,
+    /// Initialize and run the allocator
+    #[arg(long)]
+    init: bool,
     /// Where to store the benchmark results in csv format.
-    #[arg(short, long, default_value = "bench/out/bench.csv")]
+    #[arg(short, long, default_value = "")]
     outfile: String,
-
 }
 
 type Allocator = Array<3, Cache<32>>;
@@ -46,15 +51,42 @@ fn main() {
         memory,
         threads,
         crash,
+        init,
         outfile,
     } = Args::parse();
 
     assert!(memory > 0 && threads > 0);
 
-    let mut mapping = map(0x1000_0000_0000, memory * PT_LEN * PT_LEN, &dax).unwrap();
+    if init {
+        initialize(memory, &dax, threads, crash);
+    } else {
+        let mut command = Command::new(std::env::current_exe().unwrap());
+        command.args([
+            "--init",
+            "--dax",
+            &dax,
+            &format!("-m{memory}"),
+            &format!("-t{threads}"),
+        ]);
+        if crash {
+            command.arg("--crash");
+        }
+        let mut process = command.spawn().unwrap();
+        if crash {
+            std::thread::sleep(Duration::from_secs(5));
+            process.kill().unwrap();
+        } else {
+            let ret = process.wait().unwrap();
+            assert!(ret.success());
+        }
 
+        recover(threads, memory, &dax, &outfile);
+    }
+}
+
+fn initialize(memory: usize, dax: &String, threads: usize, crash: bool) {
+    let mut mapping = map(0x1000_0000_0000, memory * PT_LEN * PT_LEN, dax).unwrap();
     let alloc = Allocator::new(threads, &mut mapping, Init::Overwrite, false).unwrap();
-
     warn!("Prepare alloc");
     thread::parallel(
         mapping[..alloc.pages()]
@@ -72,47 +104,41 @@ fn main() {
             for page in pages.into_iter().take(frees) {
                 alloc.put(t, page, 0).unwrap()
             }
+
+            if crash {
+                loop {
+                    let page = alloc.get(t, 0).unwrap();
+                    black_box(page);
+                    alloc.put(t, page, 0).unwrap();
+                }
+            }
         },
     );
+    assert!(!crash);
     let num_allocated = alloc.dbg_allocated_pages();
     warn!("Allocated: {num_allocated}");
     assert!(0 < num_allocated && num_allocated < alloc.pages());
+}
 
-    if crash {
-        // Invalidate some counters
-        for t in 0..threads {
-            let start = alloc.pages() / threads * t;
-
-            let children = start / Cache::<32>::N;
-            let bitfield = (start / PT_LEN) % 32;
-            alloc.lower.tables[children][bitfield]
-                .fetch_update(|v| v.dec(1))
-                .unwrap();
-        }
-
-        std::mem::forget(alloc); // Don't clear active flag!
-    } else {
-        drop(alloc); // Normal shutdown
-    }
+fn recover(threads: usize, memory: usize, dax: &str, outfile: &str) {
+    let mut mapping = map(0x1000_0000_0000, memory * PT_LEN * PT_LEN, &dax).unwrap();
 
     warn!("Recover alloc");
-
     let timer = Instant::now();
     let alloc = Allocator::new(threads, &mut mapping, Init::Recover, false).unwrap();
     let time = timer.elapsed().as_nanos();
 
-    warn!("Recovered {num_allocated} allocations in {time} ns");
-    assert!(alloc.dbg_allocated_pages() == num_allocated);
-
-    drop(alloc);
-    drop(mapping);
+    let num_alloc = alloc.dbg_allocated_pages();
+    warn!("Recovered {num_alloc} allocations in {time} ns");
+    let expected = alloc.pages() / 2;
+    assert!(expected - threads <= num_alloc && num_alloc <= expected + threads);
 
     let mut out = File::create(outfile).unwrap();
     writeln!(out, "recovery\n{time}").unwrap();
 }
 
 #[allow(unused_variables)]
-fn map(begin: usize, length: usize, dax: &str) -> core::result::Result<MMap<Page>, ()> {
+fn map(begin: usize, length: usize, dax: &str) -> Result<MMap<Page>, ()> {
     #[cfg(target_os = "linux")]
     {
         warn!(
